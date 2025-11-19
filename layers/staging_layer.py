3ef25735ce075eda3da5e1137b823cfc27875861
ursaslam <ursaslam@gmail.com>
2025-11-19 09:56:23 -0500

    Implement write_staging function in staging_layer
    Added function to write DataFrame to staging area with logging.

--- layers/staging_layer.py
+++ layers/staging_layer.py
@@ -1,1 +1,12 @@
-print('staging layer placeholder')
+import pyspark.sql.functions as F
+from utilities.logger import log
+
+def write_staging(df, staging_path, full_load):
+    mode = "overwrite" if full_load else "append"
+
+    df2 = df.withColumn("load_dt", F.current_date()) \
+            .withColumn("load_hr", F.hour(F.current_timestamp()))
+
+    df2.write.mode(mode).partitionBy("load_dt", "load_hr").parquet(staging_path)
+
+    log("info", "staging", "Data written to staging", path=staging_path)
