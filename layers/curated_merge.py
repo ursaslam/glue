1f796030e9362f5840a3c78eda24ef42a23abbd3
ursaslam <ursaslam@gmail.com>
2025-11-19 09:56:39 -0500

    Implement curated DataFrame write functionality
    Added function to write curated DataFrame to parquet.

--- layers/curated_merge.py
+++ layers/curated_merge.py
@@ -1,1 +1,6 @@
-print('curated merge placeholder')
+from utilities.logger import log
+
+def write_curated(df, curated_path, full_load):
+    mode = "overwrite" if full_load else "append"
+    df.write.mode(mode).parquet(curated_path)
+    log("info", "curated", "Curated write done", mode=mode)
