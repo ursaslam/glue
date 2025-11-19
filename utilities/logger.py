3fb02c75392922ef1ac2fa27818c53fb633f8b01
ursaslam <ursaslam@gmail.com>
2025-11-19 09:53:50 -0500

    Implement logging function in logger.py

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,1 +1,12 @@
-print('logger placeholder')
+import json
+from datetime import datetime
+
+def log(level, stage, message, **details):
+    entry = {
+        "timestamp": datetime.utcnow().isoformat(),
+        "level": level.upper(),
+        "stage": stage,
+        "message": message,
+        "details": details
+    }
+    print(json.dumps(entry))
